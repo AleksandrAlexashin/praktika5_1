448b6a1cf7ac5c012f963874d1f066d748aa2578
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:26:31 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass1 = new int[7];
+            double s = 0;
+            
+            Console.WriteLine("Введите элементы массива ");
+            for (int i=0; i<7; i++)
+            {
+                mass1[i] = Convert.ToInt32(Console.ReadLine());
+                 s+= mass1[i];
+                            }
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("|" + mass1[i] + "|");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое элементов равно {0:f2}" , s / 7) ;
+            Console.ReadKey();
+        }
+        
+    }
+    
+    
+}
